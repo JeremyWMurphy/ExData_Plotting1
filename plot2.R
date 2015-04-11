bee6cd3508b9fb387e07ef0d685ec70d72a256ab
JeremyWMurphy <JeremyWMurphy@gmail.com>
2015-04-11 16:50:08 -0400

    Initial commit after fork

---
+++ plot2.R
@@ -0,0 +1,21 @@
+plot2 <- function(){
+  
+  # read in data, assumes text file is in the working directory
+  
+  ftxt <- "household_power_consumption.txt"
+  
+  varnames <- colnames(read.table(file = ftxt, header = TRUE, sep = ";", nrows = 1)) # column names
+  
+  dat <- read.table(file = ftxt, header = FALSE, sep = ";", na.strings = "?", col.names = varnames, nrows = 2879, skip = 66638) # read only a subset of the data, corresponding to measurements from dates 2007-2-1 through 2007-2-2
+  
+  #format date variable
+  dat$Date <- as.Date(dat$Date,format = "%d/%m/%Y")
+  dat$datetime <- paste(dat$Date,dat$Time)
+  dat$datetime <- strptime(dat$datetime,format = "%Y-%m-%d %H:%M:%S")
+  
+  # create plot
+  png(filename = "plot2.png")
+  plot(dat$datetime,dat$Global_active_power,type = "l",col = "black",ylab = "Global Active Power (kilowatts)",xlab = "")
+  dev.off()
+  
+}
---
+++ plot3.R
@@ -0,0 +1,24 @@
+plot3 <- function(){
+  
+  # read in data, assumes text file is in the working directory
+  
+  ftxt <- "household_power_consumption.txt"
+  
+  varnames <- colnames(read.table(file = ftxt, header = TRUE, sep = ";", nrows = 1)) # column names
+  
+  dat <- read.table(file = ftxt, header = FALSE, sep = ";", na.strings = "?", col.names = varnames, nrows = 2879, skip = 66638) # read only a subset of the data, corresponding to measurements from dates 2007-2-1 through 2007-2-2
+  
+  #format date variable
+  dat$Date <- as.Date(dat$Date,format = "%d/%m/%Y")
+  dat$datetime <- paste(dat$Date,dat$Time)
+  dat$datetime <- strptime(dat$datetime,format = "%Y-%m-%d %H:%M:%S")
+  
+  # create plot
+  png(filename = "plot3.png")
+  plot(dat$datetime,dat$Sub_metering_1,type = "l",col = "black",ylab = "Energy sub metering",xlab = "")
+  lines(dat$datetime,dat$Sub_metering_2,col = "red")
+  lines(dat$datetime,dat$Sub_metering_3,col = "blue")
+  legend(x = "topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), pch = -1 ,lty = 1, col = c("black","red","blue"))
+  dev.off()
+  
+}
